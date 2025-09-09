de02514376ed4b2f9a21c21a5abee2f2bac1d67d
Quentin Chatagny <e.qchatagny2@etu.cvm.qc.ca>
2025-09-09 10:40:47 -0400

    algorithme functions et reflecteur

--- enigma.py
+++ enigma.py
@@ -1,6 +1,39 @@
 #enigma1
 
-def printf():
-  print("hello world")
+#ENCODE
 
-#asdafs
+#FUNCTION#################################
+#Def:
+#Params:
+#return:
+#rtype:
+#########################################
+def moteur(Cles, Decs, Ref, isEncoding):
+  if (isEncoding):
+    encode(Cles, Decs, Ref)
+  else:
+    decode(Cles, Decs, Ref)
+
+#FUNCTION#################################
+#Def:
+#Params:
+#return:
+#rtype:
+#########################################
+def encode(Cles, Decs, Ref):
+  pass
+
+#FUNCTION#################################
+#Def:
+#Params:
+#return:
+#rtype:
+#########################################
+def decode(Cles, Decs, ref):
+  pass
+
+
+
+
+
+
