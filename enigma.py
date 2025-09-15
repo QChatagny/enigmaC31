1e707937d0e09710ecb7516200b546f866f19a5c
filou <larosefelixb@gmail.com>
2025-09-15 11:10:35 -0400

    premier jet de l'affichage

--- enigma.py
+++ enigma.py
@@ -1,8 +1,37 @@
 #enigma1
 
-def printf():
-  print("hello world")
+# CODE POUR L'INTERFACE
 
-#asdafs
+# VARIABLES
+valeurBaseCles = []
+valeurDecalage = []
+messageCode = ""
+messageDecoder = ""
+reponseValide = True
+nombreCles = ["1er", "2e", "3e", "4e"]
 
-# felix's branch test
+def encoder_Affichage():
+  while nombreCles <= 3:
+    iterationCles = 0
+    int(input(print(f"Quelle est la valeur de base de la {nombreCles[iterationCles]} ? ")))
+    
+
+def decoder_Affichage():
+  print("zeub")
+
+def question_de_depart():
+  print("Bienvenue sur ENIGMA ! \n\n")
+  while reponseValide != True:
+    rep = chr(input("Souhaiter vous encoder (e) ou decoder (d) ou quitter (q) ? "))
+    if rep == 'q':
+      quitter = True
+    elif rep == 'e':
+      encoder_Affichage()
+    elif rep == 'd':
+      decoder_Affichage()
+    else:
+      reponseValide = False
+      print("Pas un choix valide ressayer \n\n")
+
+
+      
